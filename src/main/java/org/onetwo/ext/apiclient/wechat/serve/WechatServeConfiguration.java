d0a2007d5fc7277223c15d3f3bcd3c18b8416d2d
wayshall <weishao.zeng@gmail.com>
2017-10-25 09:58:19 +0800

    bugfix

--- src/main/java/org/onetwo/ext/apiclient/wechat/serve/WechatServeConfiguration.java
+++ src/main/java/org/onetwo/ext/apiclient/wechat/serve/WechatServeConfiguration.java
@@ -1,6 +1,5 @@
 package org.onetwo.ext.apiclient.wechat.serve;
 
-import org.onetwo.common.spring.condition.OnMissingBean;
 import org.onetwo.ext.apiclient.wechat.core.DefaultWechatConfig;
 import org.onetwo.ext.apiclient.wechat.serve.service.HtppSessionStoreService;
 import org.onetwo.ext.apiclient.wechat.serve.service.MessageRouterServiceImpl;
@@ -8,6 +7,7 @@ import org.onetwo.ext.apiclient.wechat.serve.spi.ServeEndpoint;
 import org.onetwo.ext.apiclient.wechat.serve.spi.WechatSessionRepository;
 import org.onetwo.ext.apiclient.wechat.serve.web.ServeController;
 import org.onetwo.ext.apiclient.wechat.serve.web.WechatOAuth2Hanlder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -25,20 +25,20 @@ public class WechatServeConfiguration  {
 	
 	@Bean
 //	@Conditional(MissingServeEndpointCondition.class)
-	@OnMissingBean(ServeEndpoint.class)
+	@ConditionalOnMissingBean(ServeEndpoint.class)
 	public ServeEndpoint serveEndpoint(){
 		return new ServeController();
 	}
 	
 
 	@Bean
-	@OnMissingBean(WechatOAuth2Hanlder.class)
+	@ConditionalOnMissingBean(WechatOAuth2Hanlder.class)
 	public WechatOAuth2Hanlder wechatOAuth2Hanlder(){
 		return new WechatOAuth2Hanlder();
 	}
 	
 	@Bean
-	@OnMissingBean(WechatSessionRepository.class)
+	@ConditionalOnMissingBean(WechatSessionRepository.class)
 	public WechatSessionRepository sessionStoreService(){
 		return new HtppSessionStoreService();
 	}
