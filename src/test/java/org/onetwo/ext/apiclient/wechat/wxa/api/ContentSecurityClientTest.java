9750089c72955a6d5c056f9f91bd635ef2c8aac4
wayshall <weishao.zeng@gmail.com>
2018-07-13 18:58:31 +0800

    addd test

--- src/test/java/org/onetwo/ext/apiclient/wechat/wxa/api/ContentSecurityClientTest.java
+++ src/test/java/org/onetwo/ext/apiclient/wechat/wxa/api/ContentSecurityClientTest.java
@@ -4,11 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import org.junit.Test;
+import org.onetwo.common.apiclient.resouce.FileNameByteArrayResource;
 import org.onetwo.common.exception.ApiClientException;
+import org.onetwo.common.file.FileUtils;
 import org.onetwo.ext.apiclient.wechat.WechatBaseTestsAdapter;
 import org.onetwo.ext.apiclient.wechat.basic.response.WechatResponse;
 import org.onetwo.ext.apiclient.wechat.utils.AccessTokenInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 
@@ -36,6 +39,17 @@ public class ContentSecurityClientTest extends WechatBaseTestsAdapter {
 	}
 	
 	@Test
+	public void testImgSecCheckWithBytes() throws Exception{
+		AccessTokenInfo accessToken = getAccessToken();
+		System.out.println("tokenInfo:"+accessToken);
+		Resource resource = new FileSystemResource("d:/test/test.jpg");
+		ByteArrayResource media = new FileNameByteArrayResource("test.jpg", FileUtils.toByteArray(resource.getInputStream()));
+		WechatResponse res = contentSecurityClient.imgSecCheck(accessToken, media);
+		System.out.println("res:" + res);
+		assertThat(res.isSuccess()).isTrue();
+	}
+	
+	@Test
 	public void testMsgSecCheck() throws Exception{
 		String content = "我是党员老江";
 		AccessTokenInfo accessToken = getAccessToken();
@@ -48,5 +62,6 @@ public class ContentSecurityClientTest extends WechatBaseTestsAdapter {
 		res = contentSecurityClient.msgSecCheck(accessToken, "我是党员江他喵的泽民");
 		assertThat(res.isSuccess()).isTrue();
 	}
+	
 
 }
