b4abb4f2e93ba1d1e30d0558b2299a963df3a3db
wayshall <weishao.zeng@gmail.com>
2019-04-11 15:04:19 +0800

    fix: oauth2 state store

--- src/main/java/org/onetwo/ext/apiclient/wechat/serve/service/HttpRequestStoreService.java
+++ src/main/java/org/onetwo/ext/apiclient/wechat/serve/service/HttpRequestStoreService.java
@@ -1,11 +1,14 @@
 package org.onetwo.ext.apiclient.wechat.serve.service;
 
 import java.util.Optional;
+import java.util.UUID;
 
+import org.onetwo.boot.module.redis.TokenValidator;
 import org.onetwo.ext.apiclient.wechat.serve.dto.WechatOAuth2Context;
 import org.onetwo.ext.apiclient.wechat.serve.spi.WechatOAuth2UserRepository;
 import org.onetwo.ext.apiclient.wechat.serve.spi.WechatOAuth2UserRepository.OAuth2User;
 import org.onetwo.ext.apiclient.wechat.utils.WechatConstants.Oauth2ClientKeys;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import lombok.Setter;
 
@@ -18,8 +21,8 @@ import lombok.Setter;
 public class HttpRequestStoreService<T extends OAuth2User> implements WechatOAuth2UserRepository<T> {
 	@Setter
 	private String userInfoKey = Oauth2ClientKeys.STORE_USER_INFO_KEY;
-	/*@Autowired
-	private TokenValidator tokenValidator;*/
+	@Autowired
+	private TokenValidator tokenValidator;
 	
 	@SuppressWarnings("unchecked")
 	@Override
@@ -30,5 +33,27 @@ public class HttpRequestStoreService<T extends OAuth2User> implements WechatOAut
 	@Override
 	public void saveCurrentUser(WechatOAuth2Context request, T userInfo, boolean refresh){
 		request.getRequest().setAttribute(userInfoKey, userInfo);
+	}
+	
+	public boolean checkOauth2State(WechatOAuth2Context request) {
+		String key = getStateKey(request.getState());
+		boolean check = tokenValidator.check(key, request.getState(), false);
+		return check;
+	}
+
+	protected String getStateKey(String state) {
+		return "wechat_oauth2_state:" + state;
+	}
+	/****
+	 * 生成state参数
+	 * @author wayshall
+	 * @param request
+	 * @return
+	 */
+	public String generateAndStoreOauth2State(WechatOAuth2Context request){
+		String state = UUID.randomUUID().toString();
+		String key = getStateKey(state);
+		this.tokenValidator.save(key, () -> state);
+		return state;
 	}
 }
