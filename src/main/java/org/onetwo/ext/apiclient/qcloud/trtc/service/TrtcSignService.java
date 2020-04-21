1eae9cc913e57c3e28b375a7c411183c30b25576
wayshall <weishao.zeng@gmail.com>
2020-04-21 13:32:06 +0800

    fix sign

--- src/main/java/org/onetwo/ext/apiclient/qcloud/trtc/service/TrtcSignService.java
+++ src/main/java/org/onetwo/ext/apiclient/qcloud/trtc/service/TrtcSignService.java
@@ -4,6 +4,7 @@ package org.onetwo.ext.apiclient.qcloud.trtc.service;
  * <br/>
  */
 
+import org.apache.commons.lang3.StringUtils;
 import org.onetwo.common.utils.Assert;
 import org.onetwo.ext.apiclient.qcloud.trtc.TrtcProperties;
 import org.springframework.beans.factory.InitializingBean;
@@ -38,6 +39,46 @@ public class TrtcSignService implements InitializingBean {
 	 */
 	public String genSig(String userId, Long expireInSeconds) {
 		return this.tlsSigApi.genSig(userId, expireInSeconds!=null?expireInSeconds:trtcProperties.getDefaultExpireTime());
+	}
+	
+	/***
+	 * 复制自微信小程序sdk客户端，保持一致
+	 * @author weishao zeng
+	 * @param streamUrl
+	 * @return
+	 */
+	public static String getStreamIDByStreamUrl(String streamUrl) {
+	    if (StringUtils.isBlank(streamUrl)) {
+	        return null;
+	    }
+	    //推流地址格式: rtmp://8888.livepush.myqcloud.com/path/8888_test_12345?txSecret=aaa&txTime=bbb
+	    //拉流地址格式: rtmp://8888.livepush.myqcloud.com/path/8888_test_12345
+	    //             http://8888.livepush.myqcloud.com/path/8888_test_12345.flv
+	    //             http://8888.livepush.myqcloud.com/path/8888_test_12345.m3u8
+
+	    String subStr = streamUrl;
+	    int index = subStr.indexOf('?');
+	    if (index >= 0) {
+	        subStr = subStr.substring(0, index);
+	    }
+	    if (StringUtils.isBlank(subStr)) {
+	        return null;
+	    }
+	    index = subStr.lastIndexOf('/');
+	    if (index >= 0) {
+	        subStr = subStr.substring(index + 1);
+	    }
+	    if (StringUtils.isBlank(subStr)) {
+	        return null;
+	    }
+	    index = subStr.indexOf('.');
+	    if (index >= 0) {
+	        subStr = subStr.substring(0, index);
+	    }
+	    if (StringUtils.isBlank(subStr)) {
+	        return null;
+	    }
+	    return subStr;
 	}
 	
 }
