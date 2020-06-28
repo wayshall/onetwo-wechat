3ac7df2d5667bfa3a43ed5e935b94a2f8df50d82
wayshall <weishao.zeng@gmail.com>
2020-06-28 23:15:45 +0800

    fix qcloud

--- src/main/java/org/onetwo/ext/apiclient/qcloud/QCloudProperties.java
+++ src/main/java/org/onetwo/ext/apiclient/qcloud/QCloudProperties.java
@@ -2,6 +2,10 @@ package org.onetwo.ext.apiclient.qcloud;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import com.tencentcloudapi.common.Credential;
+import com.tencentcloudapi.common.profile.ClientProfile;
+import com.tencentcloudapi.common.profile.HttpProfile;
+
 import lombok.Data;
 
 /**
@@ -17,4 +21,32 @@ public class QCloudProperties {
 	//腾讯云账户secretId，secretKey
 	String secretId;
 	String secretKey;
+	String region = "ap-guangzhou";
+	String signMethod = ClientProfile.SIGN_TC3_256;
+	
+	/****
+	 * https://github.com/TencentCloud/tencentcloud-sdk-java
+	 * 
+	 * @author weishao zeng
+	 * @return
+	 */
+	public Credential newCredential() {
+        Credential credential = new Credential(secretId, secretKey);
+        return credential;
+	}
+	/***
+	 * 创建 签名方法 v3
+	 * @author weishao zeng
+	 * @return
+	 */
+	public ClientProfile newLiveClientProfile() {
+        HttpProfile httpProfile = new HttpProfile();
+        httpProfile.setEndpoint("live.tencentcloudapi.com");
+        
+		ClientProfile clientProfile = new ClientProfile();
+        clientProfile.setSignMethod(signMethod);
+        clientProfile.setHttpProfile(httpProfile);
+        
+        return clientProfile;
+	}
 }
