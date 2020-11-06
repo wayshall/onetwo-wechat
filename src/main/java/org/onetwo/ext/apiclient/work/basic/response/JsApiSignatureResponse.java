9bf578bdc76a58806ecae3f3825dfe96f7415675
wayshall <weishao.zeng@gmail.com>
2020-11-06 11:56:43 +0800

    fix wechat sign

--- src/main/java/org/onetwo/ext/apiclient/work/basic/response/JsApiSignatureResponse.java
+++ src/main/java/org/onetwo/ext/apiclient/work/basic/response/JsApiSignatureResponse.java
@@ -13,6 +13,16 @@ public class JsApiSignatureResponse {
     private Long timestamp;// 必填，生成签名的时间戳
     private String noncestr; // 必填，生成签名的随机串
     private String signature;
+    
+    /***
+     * js端nonceStr字段是驼峰命名，为了避免坑，多返回一个驼峰命名的字段
+     */
+	public String getNonceStr() {
+		return noncestr;
+	}
+	public void setNonceStr(String nonceStr) {
+		this.noncestr = nonceStr;
+	}
 
 }
 
