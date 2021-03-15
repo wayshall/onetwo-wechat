81c7b4f010895d143851307bc4a50798d36b795d
weishao zeng <weishao.zeng@gmail.com>
2021-03-15 18:24:59 +0800

    fix wechat access token service

---
+++ src/main/java/org/onetwo/ext/apiclient/wechat/core/AccessTokenParameterTypes.java
@@ -0,0 +1,17 @@
+package org.onetwo.ext.apiclient.wechat.core;
+/**
+ * access_token参数类型
+ * @author weishao zeng
+ * <br/>
+ */
+public enum AccessTokenParameterTypes {
+	/***
+	 * 直接附加到url参数
+	 */
+	URL,
+	/***
+	 * header
+	 */
+	HEADER
+
+}
