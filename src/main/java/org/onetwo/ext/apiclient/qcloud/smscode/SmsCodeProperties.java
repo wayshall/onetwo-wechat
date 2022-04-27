4ce8840959384bfd8e47b56471d652760d2092c9
weishao zeng <weishao.zeng@gmail.com>
2022-04-27 11:40:46 +0800

    fix: fix smscode key

--- src/main/java/org/onetwo/ext/apiclient/qcloud/smscode/SmsCodeProperties.java
+++ src/main/java/org/onetwo/ext/apiclient/qcloud/smscode/SmsCodeProperties.java
@@ -12,7 +12,7 @@ import lombok.Data;
 @Data
 public class SmsCodeProperties {
 	
-	public static final String PREFIX = "qcloud.smsCode";
+	public static final String PREFIX = "qcloud.smscode";
 	/***
 	 * 恢复使用 qcloud.smsCode.enabled 来控制是否启用sms相关服务，因为有时候 template-id 并不是静态配置的，不是使用通过此属性来判断是否启用
 	 */
@@ -25,7 +25,7 @@ public class SmsCodeProperties {
 	// @Deprecated 有时候 template-id 并不是静态配置的，不是使用通过此属性来判断是否启用
 //	public static final String SMS_SERVICE_ENABLE_KEY = PREFIX + ".template-id";
 
-	private String storeKey = "smsCode";
+	private String storeKey = "smscode";
 	private int codeLength = 4;
 	private int validInMinutes = 2;
 	private int templateId;
